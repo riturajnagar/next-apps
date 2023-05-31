5779b1db9b1a87bce3608364e259e8a63f3cf36c
riturajnagar <riturajnagar78@gmail.com>
2023-05-31 17:11:23 -0400

    blog integration done

---
+++ blog-app/src/pages/[slug].js
@@ -0,0 +1,44 @@
+import ReactMarkdown from 'react-markdown'
+import { getAllPublished, getSinglePost } from "./notion"
+
+const Post = ({ post }) => {
+    return (
+        <section className='container my-3'>
+            <h2 className='text-center'>{post.metadata.title}</h2>
+            <span>{post.metadata.date}</span>
+            <div className="container my-3 border shadow-sm p-3 mb-5 bg-body-tertiary rounded">
+                <p className='text-center word-wrap'>{post.metadata.description}</p>
+            </div>
+
+            <ReactMarkdown>{post.markdown}</ReactMarkdown>
+        </section>
+    );
+};
+/*
+Similarly to the blog overview page, you will be pre-rendering each post page.
+ 
+In /pages/posts/[slug].js, add the getStaticProps() function after the Post component and call the getSingleBlogPostBySlug function to fetch the blog post from Notion.
+*/
+
+export const getStaticProps = async ({ params }) => {
+    const post = await getSinglePost(params.slug)
+
+    return {
+        props: {
+            post,
+        },
+        revalidate: 60
+    };
+};
+
+export const getStaticPaths = async () => {
+    const posts = await getAllPublished();
+    const paths = posts.map(({ slug }) => ({ params: { slug } }));
+
+    return {
+        paths,
+        fallback: "blocking",
+    };
+};
+
+export default Post;
---
+++ blog-app/src/pages/_app.js
@@ -0,0 +1,11 @@
+import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import { useEffect } from 'react';
+
+
+export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, [])
+  return <Component {...pageProps} />
+}
